bb6c2933e4ab078635248398c63ba9c5f0516a50
Marcin Skic <marcinskicoficjal@gmail.com>
2023-06-19 21:16:29 +0200

    Finish chapter 3.1

---
+++ variables/src/main.rs
@@ -0,0 +1,25 @@
+#![allow(dead_code)]
+
+fn main() {
+    shadowing()
+}
+
+fn mutableness() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+
+fn shadowing() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
