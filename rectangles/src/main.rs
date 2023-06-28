9b73472a67f1a3f8e7449c47728639bdbfb44fdc
Marcin Skic <marcinskicoficjal@gmail.com>
2023-06-28 13:42:04 +0200

    Finish chapter 5

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,11 +4,44 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn set_width(&mut self, width: u32) {
+        self.width = width;
+    }
+
+    fn max(self, other: Rectangle) -> Rectangle {
+        Rectangle {
+            width: self.width.max(other.width),
+            height: self.height.max(other.height),
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle::square(60);
 
     println!("rect1 is {:#?}", rect1);
 
@@ -16,10 +49,54 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area() // also correct: Rectangle::area(&rect1)
     );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    syntactic_sugar_proof();
+    consuming_self();
+    auto_referencing_sugar();
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn syntactic_sugar_proof() {
+    let r = &mut Box::new(Rectangle {
+        width: 1,
+        height: 2,
+    });
+    let area1 = r.area();
+    let area2 = Rectangle::area(&**r);
+    assert_eq!(area1, area2);
+}
+
+fn consuming_self() {
+    let rect = Rectangle {
+        width: 0,
+        height: 0,
+    };
+    println!("{}", rect.area());
+
+    let other_rect = Rectangle {
+        width: 1,
+        height: 1,
+    };
+    let max_rect = rect.max(other_rect);
+    //rect and other_rect doesn't exist anymore at this point;
+    println!("{:#?}", max_rect);
+}
+
+fn auto_referencing_sugar() {
+    // Added the mut keyword to the let-binding
+    let mut rect = Rectangle {
+        width: 0,
+        height: 0,
+    };
+
+    // same operations
+    rect.set_width(1);
+    (&mut rect).set_width(1);
+    Rectangle::set_width(&mut rect, 1);
+
+    println!("{:#?}", rect);
 }
