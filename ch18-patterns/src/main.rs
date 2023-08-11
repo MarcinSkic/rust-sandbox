1d691e69ce4a5b5342e10d59c0910bcc02e54a65
Marcin Skic <marcinskicoficjal@gmail.com>
2023-08-11 23:11:06 +0200

    Finish chapter 18.3

--- ch18-patterns/src/main.rs
+++ ch18-patterns/src/main.rs
@@ -72,7 +72,166 @@ fn main() {
 
     // let can have refutable pattern but then must panic or return in case of failure
     {
-        let some_option_value: Option<i32> = None;
+        let some_option_value: Option<i32> = Some(5);
         let Some(_x) = some_option_value else { todo!("The hell") };
+    }
+
+    // shadowing variables
+    {
+        let x = Some(5);
+        let y = 10;
+
+        match x {
+            Some(50) => println!("Got 50"),
+            Some(y) => println!("Matched, y = {y}"),
+            _ => println!("Default case, x = {:?}", x),
+        }
+
+        println!("at the end: x = {:?}, y = {y}", x);
+    }
+
+    // multiple patterns
+    {
+        let x = 1;
+
+        match x {
+            1 | 2 => println!("one or two"),
+            3 => println!("three"),
+            _ => println!("anything"),
+        }
+    }
+
+    // ranges (only for numbers and chars)
+    {
+        let x = 5;
+
+        match x {
+            1..=5 => println!("one through five"),
+            _ => println!("something else"),
+        }
+    }
+
+    // destructuring structs
+    {
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        let p = Point { x: 0, y: 7 };
+
+        let Point { x: a, y: b } = p;
+        assert_eq!(0, a);
+        assert_eq!(7, b);
+    }
+
+    // destructuring with literal values
+    {
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        let p = Point { x: 0, y: 7 };
+
+        match p {
+            Point { x: 0, y: 0 } => println!("On the origin (0,0)"),
+            Point { x, y: 0 } => println!("On the x axis at {x}"),
+            Point { x: 0, y } => println!("On the y axis at {y}"),
+            Point { x, y } => {
+                println!("On neither axis: ({x}, {y})");
+            }
+        }
+    }
+
+    // destructuring enum with different types of values, also nested structs
+    #[allow(dead_code)]
+    {
+        enum Color {
+            Rgb(i32, i32, i32),
+            Hsv(i32, i32, i32),
+        }
+
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(Color),
+        }
+
+        let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+        match msg {
+            Message::Quit => {
+                println!("The Quit variant has no data to destructure.");
+            }
+            Message::Move { x, y } => {
+                println!("Move in the x direction {x} and in the y direction {y}");
+            }
+            Message::Write(text) => {
+                println!("Text message: {text}");
+            }
+            Message::ChangeColor(Color::Rgb(r, g, b)) => {
+                println!("Change color to red {r}, green {g}, and blue {b}");
+            }
+            Message::ChangeColor(Color::Hsv(h, s, v)) => {
+                println!("Change color to hue {h}, saturation {s}, value {v}")
+            }
+        }
+    }
+
+    // ignoring values
+    {
+        let mut setting_value = Some(5);
+        let new_setting_value = Some(10);
+
+        match (setting_value, new_setting_value) {
+            (Some(_), Some(_)) => {
+                println!("Can't overwrite an existing customized value");
+            }
+            _ => {
+                setting_value = new_setting_value;
+            }
+        }
+
+        println!("setting is {:?}", setting_value);
+    }
+
+    // values ignored with _ doesn't bind at all
+    {
+        let s = Some(String::from("Hello!"));
+
+        if let Some(_) = s {
+            println!("found a string");
+        }
+
+        println!("{:?}", s);
+    }
+
+    // ignoring multiple values
+    #[allow(dead_code)]
+    {
+        struct Point {
+            x: i32,
+            y: i32,
+            z: i32,
+        }
+
+        let origin = Point { x: 0, y: 0, z: 0 };
+
+        match origin {
+            Point { x, .. } => println!("x is {}", x),
+        }
+    }
+
+    // match guards
+    {
+        let num = Some(4);
+
+        match num {
+            Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+            Some(x) => println!("The number {} is odd", x),
+            None => (),
+        }
     }
 }
