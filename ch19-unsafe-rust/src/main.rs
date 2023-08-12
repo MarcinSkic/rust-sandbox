f7422329e2ecba555486c8a0be4fdd14308d80e9
Marcin Skic <marcinskicoficjal@gmail.com>
2023-08-12 14:59:32 +0200

    Finish chapter 19.1

---
+++ ch19-unsafe-rust/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    // raw pointers
+    {
+        let mut num = 5;
+
+        let r1 = &num as *const i32;
+        let r2 = &mut num as *mut i32;
+
+        unsafe {
+            println!("r1 is: {}", *r1);
+            println!("r2 is: {}", *r2);
+        }
+    }
+
+    // closing unsafe code in safe abstraction
+    {
+        use std::slice;
+
+        fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+            let len = values.len();
+            let ptr = values.as_mut_ptr();
+
+            assert!(mid <= len);
+
+            unsafe {
+                (
+                    slice::from_raw_parts_mut(ptr, mid),
+                    slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+                )
+            }
+        }
+
+        let mut v = vec![1, 2, 3, 4, 5, 6];
+        let r = &mut v[..];
+
+        let (a, b) = split_at_mut(r, 3);
+
+        assert_eq!(a, &mut [1, 2, 3]);
+        assert_eq!(b, &mut [4, 5, 6]);
+    }
+
+    // accesing or modifying mutable static variables is always unsafe
+    {
+        static mut COUNTER: u32 = 0;
+
+        fn add_to_count(inc: u32) {
+            unsafe {
+                COUNTER += inc;
+            }
+        }
+
+        add_to_count(3);
+
+        unsafe {
+            println!("COUNTER: {}", COUNTER);
+        }
+    }
+}
